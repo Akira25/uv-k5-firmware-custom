20f6fa99281927ccddeb95882e75730674163589
Martin Hübner <martin.hubner@web.de>
2024-03-29 14:43:47 +0100

    add custom build command

--- compile-with-docker.sh
+++ compile-with-docker.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 docker build -t uvk5 .
 docker run --rm -v ${PWD}/compiled-firmware:/app/compiled-firmware uvk5 /bin/bash -c "cd /app && make && cp firmware* compiled-firmware/"
+
+# custom build command martin
+podman run --rm -v ${PWD}:/app:z -v ${PWD}/compiled-firmware:/app/compiled-firmware:z uvk5 /bin/bash -c "cd /app && make clean && make && cp firmware* compiled-firmware/"
